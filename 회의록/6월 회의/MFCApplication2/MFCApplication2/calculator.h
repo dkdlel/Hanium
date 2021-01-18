0db037267808e8adee76b8e2dcfb3fb337dcf3fd
JeongHyun <k557330@naver.com>
2021-01-18 17:08:09 +0900

    시연영상, 결과보고서, 회의록 추가

---
+++ 회의록/6월 회의/MFCApplication2/MFCApplication2/calculator.h
@@ -0,0 +1,9 @@
+#ifndef calculator
+#define calculator
+
+int add(int, int);
+int substract(int, int);
+int multifly(int, int);
+int divide(int, int);
+
+#endif
---
+++ 회의록/6월 회의/swap/swap/swap.cpp
@@ -0,0 +1,9 @@
+﻿#include "stdafx.h"
+
+extern "C" __declspec(dllexport)
+
+void swapping(int *a, int *b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
